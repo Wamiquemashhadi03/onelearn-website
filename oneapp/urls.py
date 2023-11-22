c47b03b311289fefe83bba1773753bdd6a247f71
Wamiquemashhadi03 <96311443+Wamiquemashhadi03@users.noreply.github.com>
2023-11-22 22:44:55 +0530

    Update urls.py

--- oneapp/urls.py
+++ oneapp/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('register/', views.register, name='register'),
-    path('login',views.login, name='login'),
+    path('register/', views.register_user, name='register'),
+    path('login',views.login_user, name='login'),
     path('about', views.about, name='about'),
     path('trainers', views.trainers, name='trainers'),
     path('events', views.events, name='events'),
@@ -12,7 +12,7 @@ urlpatterns = [
     path('contact', views.contact, name='contact'),
     path('courses', views.courses, name='courses'),
     path('coursedetails', views.coursedetails, name='coursedetails'),
-    path('logout', views.logout, name='logout'),
+    path('logout', views.logout_user, name='logout'),
     path('profile', views.profile, name='profile'),
     path('editprofile', views.editprofile, name='editprofile'),
     path('index1', views.index1, name='index1'),
